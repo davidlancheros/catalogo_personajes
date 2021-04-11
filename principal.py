6390b2cf1e8530483fd034ecf384461c73bfa3f4
D@vid L@nchero$ <admin@localhost.localdomain>
2021-04-11 13:10:21 -0500

    inicio del repo

---
+++ principal.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from fabricas import *
+from random import choice
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def principal():
+    fabricas = [FabricaHumanos(), FabricaOrcos(), FabricaElfos()]
+    fabrica = choice(fabricas)
+    arma = fabrica.crear_arma()
+    escudo = fabrica.crear_escudo()
+    cuerpo = fabrica.crear_cuerpo()
+    montura = fabrica.crear_montura()
+
+    productos = []
+
+    productos.append(arma)
+    productos.append(escudo)
+    productos.append(cuerpo)
+    productos.append(montura)
+    return render_template("productos.html", productos = productos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ productos.py
@@ -0,0 +1,93 @@
+class Producto:
+    def __init__(self):
+        self.imagen = ""
+        self.descripcion = ""
+        self.grupo = ""
+
+class Arma(Producto):
+    def __init__(self):
+        Producto.__init__(self)
+
+class ArmaHumanos(Arma):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "imagenes/humanos/arma.png"
+        self.descripcion = "arma de los humanos"
+
+class ArmaOrcos(Arma):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "imagenes/orcos/arma.png"
+        self.descripcion = "arma de los orcos"
+
+class ArmaElfos(Arma):
+    def __init__(self):
+        self.grupo = "Elfos"
+        self.imagen = "imagenes/elfos/arma.png"
+        self.descripcion = "arma de los elfos"
+
+class Escudo(Producto):
+    def __init__(self):
+        Producto.__init__(self)
+
+class EscudoHumanos(Escudo):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "imagenes/humanos/escudo.png"
+        self.descripcion = "escudo de los humanos"
+
+class EscudoOrcos(Escudo):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "imagenes/orcos/escudo.png"
+        self.descripcion = "escudo de los orcos"
+
+class EscudoElfos(Escudo):
+    def __init__(self):
+        self.grupo = "Elfos"
+        self.imagen = "imagenes/elfos/escudo.png"
+        self.descripcion = "escudo de los elfos"
+
+class Cuerpo(Producto):
+    def __init__(self):
+        Producto.__init__(self)        
+
+class CuerpoHumanos(Cuerpo):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "imagenes/humanos/cuerpo.png"
+        self.descripcion = "cuerpo de los humanos"
+
+class CuerpoOrcos(Cuerpo):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "imagenes/orcos/cuerpo.png"
+        self.descripcion = "cuerpo de los orcos"
+
+class CuerpoElfos(Cuerpo):
+    def __init__(self):
+        self.grupo = "Elfos"
+        self.imagen = "imagenes/elfos/cuerpo.png"
+        self.descripcion = "cuerpo de los elfos"
+
+class Montura(Producto):
+    def __init__(self):
+        Producto.__init__(self)        
+
+class MonturaHumanos(Montura):
+    def __init__(self):
+        self.grupo = "Humanos"
+        self.imagen = "imagenes/humanos/montura.png"
+        self.descripcion = "montura de los humanos"
+
+class MonturaOrcos(Montura):
+    def __init__(self):
+        self.grupo = "Orcos"
+        self.imagen = "imagenes/orcos/montura.png"
+        self.descripcion = "montura de los orcos"
+
+class MonturaElfos(Cuerpo):
+    def __init__(self):
+        self.grupo = "Elfos"
+        self.imagen = "imagenes/elfos/montura.png"
+        self.descripcion = "montura de los elfos"        
